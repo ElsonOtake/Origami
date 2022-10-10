b94328bda4f039a9db28d3c21b381a49953369c1
Elson Otake <elsonotake@yahoo.com.br>
2022-10-10 15:53:30 -0300

    Set up Deal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_185113) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_185258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_185113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "deals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_deals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_185113) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "deals", "users"
 end
