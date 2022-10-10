811526d4d9b27718dab7ab248ce5bcbad87d8cd0
Elson Otake <elsonotake@yahoo.com.br>
2022-10-10 18:03:24 -0300

    Rollback create icon table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_185830) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_185258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,13 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_185830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_deals_on_user_id"
-  end
-
-  create_table "icons", force: :cascade do |t|
-    t.string "name"
-    t.binary "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
