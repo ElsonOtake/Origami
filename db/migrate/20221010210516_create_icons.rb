ed29e543b274d55e053b45867aa2957722b862ea
Elson Otake <elsonotake@yahoo.com.br>
2022-10-10 18:32:33 -0300

    Remove alt text column

--- db/migrate/20221010210516_create_icons.rb
+++ db/migrate/20221010210516_create_icons.rb
@@ -3,7 +3,6 @@ class CreateIcons < ActiveRecord::Migration[7.0]
     create_table :icons do |t|
       t.string :name
       t.string :file_name
-      t.string :alt_text
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_210516) do
   create_table "icons", force: :cascade do |t|
     t.string "name"
     t.string "file_name"
-    t.string "alt_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
