906652442c6d5fa8014d6b43eab825a1a51a946c
Elson Otake <elsonotake@yahoo.com.br>
2022-10-10 18:02:58 -0300

    Test image usage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories or /categories.json
   def index
     @categories = Category.all
+    @icon = Icon.last
   end
 
   # GET /categories/1 or /categories/1.json
