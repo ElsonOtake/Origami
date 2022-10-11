e1cf8414fab96b0bec273976adce033c050bbd64
Elson Otake <elsonotake@yahoo.com.br>
2022-10-11 12:16:30 -0300

    Add category and deal join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_215422) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_151423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_215422) do
     t.bigint "icon_id", null: false
     t.index ["icon_id"], name: "index_categories_on_icon_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_deals", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "deal_id", null: false
   end
 
   create_table "deals", force: :cascade do |t|
