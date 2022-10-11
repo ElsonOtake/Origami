cc8a2e25e9c28fa4d4133a6565565a6b51bd460b
Elson Otake <elsonotake@yahoo.com.br>
2022-10-11 09:47:57 -0300

    Add route to devise after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/categories', to: 'categories#index', as: 'user'
 
   # Defines the root path route ("/")
   root "categories#index"
