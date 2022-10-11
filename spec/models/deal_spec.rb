f1530c61e772c49e742d6d007e52c4dc415d879d
Elson Otake <elsonotake@yahoo.com.br>
2022-10-11 13:02:25 -0300

    Update author id tests

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Deal, type: :model do
   before(:each) do
     @author = User.new(name: 'Name Surname', email: 'deal@origami.com', password: 'password')
-    @deal = Deal.new(name: 'Deal name', amount: 25, author_id: @author.id)
+    @deal = Deal.new(name: 'Deal name', amount: 25, author: @author)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@deal).to be_valid
   end
 
   it 'is not valid without a name' do
@@ -17,7 +21,7 @@ RSpec.describe Deal, type: :model do
   end
 
   it 'is not valid without an author' do
-    @deal.author_id = nil
+    @deal.author = nil
     expect(@deal).to_not be_valid
   end
 
