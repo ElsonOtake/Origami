33639aeff83ef109c5d4b5ce75a723322bcd1f3f
Elson Otake <elsonotake@yahoo.com.br>
2022-10-11 14:02:56 -0300

    Update deals nested to categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :deals
-  resources :categories
+  resources :categories do
+    resources :deals
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/categories', to: 'categories#index', as: 'user'
