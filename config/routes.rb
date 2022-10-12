160ffcd78a459fd32503d6dd7b466905cbe78a04
Elson Otake <elsonotake@yahoo.com.br>
2022-10-12 10:46:09 -0300

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'splash/index'
   resources :categories do
     resources :deals
   end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/categories', to: 'categories#index', as: 'user'
+  get '/', to: 'splash#index'
 
   # Defines the root path route ("/")
-  root "categories#index"
+  root 'categories#index'
 end
