629afa0ec1d9200c31136c1b55d9932510ce29de
Elson Otake <elsonotake@yahoo.com.br>
2022-10-12 21:56:37 -0300

    Update Icon data

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,6 @@ class CategoriesController < ApplicationController
 
   # GET /categories/new
   def new
-    @icons = Icon.all
     @category = Category.new
   end
 
@@ -23,7 +22,6 @@ class CategoriesController < ApplicationController
 
   # POST /categories or /categories.json
   def create
-    @icons = Icon.all
     @category = Category.new(category_params)
     @category.user = current_user
 
@@ -70,6 +68,6 @@ class CategoriesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def category_params
-    params.require(:category).permit(:user_id, :name, :icon_id)
+    params.require(:category).permit(:user_id, :name, :icon)
   end
 end
