bd8fdb37a375b336d916b32232190b3de3676902
Elson Otake <elsonotake@yahoo.com.br>
2022-12-25 16:26:26 -0300

    Remove unused actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,16 +9,10 @@ class CategoriesController < ApplicationController
     @categories = customer.categories.all.order(created_at: :desc)
   end
 
-  # GET /categories/1 or /categories/1.json
-  def show; end
-
   # GET /categories/new
   def new
     @category = Category.new
   end
-
-  # GET /categories/1/edit
-  def edit; end
 
   # POST /categories or /categories.json
   def create
@@ -33,29 +27,6 @@ class CategoriesController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @category.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
