82468a4539b89cd7ecc3d7d1292fd519f5d8fd86
Elson Otake <elsonotake@yahoo.com.br>
2022-12-25 16:31:05 -0300

    Update html and json format

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,14 +19,10 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     @category.customer = current_customer
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to categories_url, notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
