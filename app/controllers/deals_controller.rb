069e00cf28639a7ba79f99c58cd4fcb699d17f79
Elson Otake <elsonotake@yahoo.com.br>
2023-07-02 19:43:30 -0300

    Remove unused code

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,41 +1,37 @@
 class DealsController < ApplicationController
-  before_action :set_deal, only: %i[show edit update destroy]
+  before_action :set_category
   before_action :authenticate_customer!
   load_and_authorize_resource
 
   # GET /deals or /deals.json
   def index
-    @category = current_customer.categories.find(params[:category_id])
     @deals = @category.deals.order(created_at: :desc)
   end
 
   # GET /deals/new
   def new
-    @category = current_customer.categories.find(params[:category_id])
     @deal = Deal.new
-    @other_categories = current_customer.categories.where.not(id: @category)
+    @other_categories = current_customer.categories.where.not(id: params[:category_id])
   end
 
   # POST /deals or /deals.json
   def create
-    @category = current_customer.categories.find(params[:category_id])
     @deal = Deal.new(deal_params)
     @deal.author = current_customer
-    @other_categories = current_customer.categories.where.not(id: @category)
     categories = Category.where(id: params[:category_ids])
     @deal.categories.push(categories)
     if @deal.save
       redirect_to category_deals_url(@category), notice: 'Transaction was successfully created.'
     else
-      redirect_to category_deals_url(@category), notice: 'Unprocessable transaction.'
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_deal
-    @deal = Deal.find(params[:id])
+  def set_category
+    @category = current_customer.categories.find(params[:category_id])
   end
 
   # Only allow a list of trusted parameters through.
