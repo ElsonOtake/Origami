a347ae788cb61820aef6f765b3c295c2b0ef5797
Elson Otake <elsonotake@yahoo.com.br>
2023-07-08 16:45:46 -0300

    Remove uniqueness validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :customer
   has_and_belongs_to_many :deals
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :icon, presence: true
 
   extend FriendlyId
