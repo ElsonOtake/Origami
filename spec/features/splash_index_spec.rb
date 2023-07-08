8a0950322e59bcde38e827f0cb0695d5cf6c21c9
Elson Otake <elsonotake@yahoo.com.br>
2023-07-08 19:10:05 -0300

    Update splash tests

--- spec/features/splash_index_spec.rb
+++ spec/features/splash_index_spec.rb
@@ -2,106 +2,121 @@ require 'rails_helper'
 
 RSpec.describe 'Splash page', type: :feature do
   describe 'without FactoryBot login' do
+    before do
+      visit root_path
+    end
+
     it 'will present splash page' do
-      visit '/'
-      expect(current_path).to eq(root_path)
+      expect(page).to have_current_path('/')
     end
 
     it 'will have page content' do
-      visit root_path
       expect(page).to have_content('Origami')
-      expect(page).to have_content('LOG IN')
-      expect(page).to have_content('SIGN UP')
+      expect(page).to have_content('Sign in')
+      expect(page).to have_content('Sign up')
       expect(page).to have_xpath("//img[@alt='House of origami']")
     end
 
-    it 'will have link to "Log in"' do
-      visit root_path
-      expect(page).to have_link('LOG IN')
+    it 'will have link to "Sign in"' do
+      expect(page).to have_link('Sign in')
     end
 
     it 'will have link to "Sign up"' do
-      visit root_path
-      expect(page).to have_link('SIGN UP')
+      expect(page).to have_link('Sign up')
     end
 
-    it 'will have classes for bootstrap cards' do
-      visit root_path
-      expect(page).to have_css('.card-body')
-      expect(page).to have_css('.card-title')
-      expect(page).to have_css('.card-image')
+    it 'will have bulma classes' do
+      expect(page).to have_css('.navbar')
+      expect(page).to have_css('.section')
+      expect(page).to have_css('.container')
+      expect(page).to have_css('.image')
+      expect(page).to have_css('.notice')
+      expect(page).to have_css('.alert')
     end
 
-    it 'will go to log in page when click on button' do
-      visit root_path
-      click_link 'LOG IN'
-      expect(current_path).to eq(new_customer_session_path)
+    it 'will not have bootstrap classes' do
+      expect(page).not_to have_css('.card-body')
+      expect(page).not_to have_css('.card-title')
+      expect(page).not_to have_css('.card-image')
+    end
+  end
+
+  describe 'User logs in' do
+    before do
+      visit new_customer_session_path
     end
 
-    it 'will have log in page content' do
-      visit root_path
-      click_link 'LOG IN'
-      expect(page).to have_content('LOGIN')
+    it 'will be in the new_customer_session_path' do
+      expect(page).to have_current_path(new_customer_session_path)
     end
 
-    it 'will have log in page with button "Log In"' do
-      visit root_path
-      click_link 'LOG IN'
+    it 'will have Login page content' do
+      expect(page).to have_content('Login')
+    end
+
+    it 'will have sign in page with button "Log In"' do
       expect(page).to have_button('Log In')
     end
 
-    it 'will have log in page with link "Forgot your password?"' do
-      visit root_path
-      click_link 'LOG IN'
+    it 'will have sign in page with button "Back"' do
+      expect(page).to have_content('Back')
+    end
+
+    it 'will have sign in page with link "Forgot your password?"' do
       expect(page).to have_link('Forgot your password?')
     end
 
-    it 'will have log in page with input fields' do
-      visit root_path
-      click_link 'LOG IN'
+    it 'will have sign in page with input fields' do
       expect(page).to have_field('Email')
       expect(page).to have_field('Password')
     end
 
-    it 'will have log in page with classes for bootstrap forms' do
-      visit root_path
-      click_link 'LOG IN'
-      expect(page).to have_css('.form-group')
-      expect(page).to have_css('.form-control')
+    it 'will have sign in page with bulma classes' do
+      expect(page).to have_css('.field')
+      expect(page).to have_css('.control')
+      expect(page).to have_css('.label')
     end
 
-    it 'will go to sign up page when click on button' do
-      visit root_path
-      click_link 'SIGN UP'
-      expect(current_path).to eq(new_customer_registration_path)
+    it 'will have sign in page without classes for bootstrap forms' do
+      expect(page).not_to have_css('.form-group')
+      expect(page).not_to have_css('.form-control')
+    end
+  end
+
+  describe 'User sign up' do
+    before do
+      visit new_customer_registration_path
+    end
+
+    it 'will go to sign up page' do
+      expect(page).to have_current_path(new_customer_registration_path)
     end
 
     it 'will have sign up page content' do
-      visit root_path
-      click_link 'SIGN UP'
-      expect(page).to have_content('REGISTER')
+      expect(page).to have_content('Sign up')
     end
 
-    it 'will have sign up page with button "Next"' do
-      visit root_path
-      click_link 'SIGN UP'
-      expect(page).to have_button('Next')
+    it 'will have sign up page with button "Back"' do
+      expect(page).to have_content('Back')
     end
 
     it 'will have sign up page with input fields' do
-      visit root_path
-      click_link 'SIGN UP'
-      expect(page).to have_field('Full name')
+      expect(page).to have_field('Name')
       expect(page).to have_field('Email')
-      expect(page).to have_field('Password (6 characters minimum)')
-      expect(page).to have_field('Password (6 characters minimum)')
+      expect(page).to have_field('Password')
+      expect(page).to have_field('Password confirmation')
+      expect(page).to have_content('6 characters minimum')
     end
 
-    it 'will have sign up page with classes for bootstrap forms' do
-      visit root_path
-      click_link 'SIGN UP'
-      expect(page).to have_css('.form-group')
-      expect(page).to have_css('.form-control')
+    it 'will have sign up page with bulma classes' do
+      expect(page).to have_css('.field')
+      expect(page).to have_css('.control')
+      expect(page).to have_css('.label')
+    end
+
+    it 'will have sign up page without classes for bootstrap forms' do
+      expect(page).not_to have_css('.form-group')
+      expect(page).not_to have_css('.form-control')
     end
   end
 end
