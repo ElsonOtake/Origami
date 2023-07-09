8eae56b85a2bdc7872d70ed8df41263b0379e167
Elson Otake <elsonotake@yahoo.com.br>
2023-07-09 09:30:14 -0300

    Add responder to devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -302,6 +302,8 @@ Devise.setup do |config|
   # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
+  config.responder.error_status = :unprocessable_entity
+  config.responder.redirect_status = :see_other
 
   # ==> Configuration for :registerable
 
